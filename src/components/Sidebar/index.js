1d7bfaf862f16deefb30924b22e1e10345fea7b7
AlphaGhostUSMC <Soulofninja69@gmail.com>
2022-04-02 09:49:17 -0700

    Added : New Logos, Tweaks

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,7 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+import LogoS from '../../assets/images/Logo-YK.png'
+import LogoSubtitle from '../../assets/images/Logo-FN.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCode, faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faDiscord, faGithub, faLinkedin, faWhatsapp, } from '@fortawesome/free-brands-svg-icons'
@@ -10,7 +10,7 @@ const Sidebar = () => (
     <div className="nav-bar">
       <Link className="logo" to="/">
          <img src={LogoS} alt="logo" />
-         <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+         <img className="sub-logo" src={LogoSubtitle} alt="yogesh" />
       </Link>
       <nav>
         <NavLink exact="true" activeclassname="active" to="/">
