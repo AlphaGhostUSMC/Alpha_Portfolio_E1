543f9ce25cd3c89d508fcd4cf1762d8a6b8193d5
AlphaGhostUSMC <Soulofninja69@gmail.com>
2022-04-06 10:18:05 -0700

    Added : Header Text Animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,39 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import LogoTitle from "../../assets/images/Logo-Y.png";
+import AnimatedLetters from "../AnimatedLetters";
 import './index.scss';
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState ('text-animate')
+  const nameArray = ['o','g','e','s','h',' ','K','u','m','a','r']
+  const jobArray = ['D','a','t','a','c','e','n','t','e','r',' ','A','r','c','h','i','t','e','c','t','.']
 
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
 
   return (
     <div className="container home-page">
       <div className="text-zone">
-        <h1>Hi,<br /> I'm
-        <img src={LogoTitle} alt="SysAdmin"/>
-        ogesh Kumar
+        <h1>
+        <span className={letterClass}>H</span>
+        <span className={`${letterClass} _12`}>i</span>
+        <span className={`${letterClass} _13`}>,</span>
         <br />
-        IT Team Lead
+        <span className={`${letterClass} _14`}>I</span>
+        <span className={`${letterClass} _15`}>'</span>
+        <span className={`${letterClass} _16`}>m</span>
+        <img src={LogoTitle} alt="name" />
+        <AnimatedLetters letterClass={letterClass} 
+        strArray={nameArray} 
+        idx={15} />
+        <br />
+        <AnimatedLetters letterClass={letterClass}
+        strArray={jobArray}
+        idx={22} />
         </h1>
         <h2>Senior System Engineer & IT Team Lead | Powershell Wizard | Python Programmer | Cloud Admin | AI Engineer</h2>
         <Link to="/contact" className='flat-button'>CONTACT ME</Link>
