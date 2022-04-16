9cf418773c444655897377014d032da8794a126c
AlphaGhostUSMC <Soulofninja69@gmail.com>
2022-04-16 08:20:55 -0700

    About Page : Added Cube Animation , Pacman Loading

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,16 +1,37 @@
+import { useEffect, useState } from 'react'
+import {
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faPython,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './index.scss'
 
 const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
   return (
-    <div className='container about-page'>
-      <div className='text-zone'>
-        <h1>
-          <AnimatedLetters
-          strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
-          idx={15}
-          />
-        </h1>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
         <p> 
             I'm very ambitious Computer Engineer looking for a role in
             established IT company with the opportunity to work with the latest
@@ -26,8 +47,33 @@ const About = () => {
             platforms. Effectively plan, install, configure and optimize IT infrastructures to achieve
             high availability and performance.
         </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faPython} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
