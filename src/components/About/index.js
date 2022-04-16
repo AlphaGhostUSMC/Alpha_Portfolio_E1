722e4eeafb788553d56cdb157eac69eeea8311a6
AlphaGhostUSMC <Soulofninja69@gmail.com>
2022-04-16 00:39:34 -0700

    About Page In Progress : text hover animation

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,21 +1,34 @@
-import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
 
 const About = () => {
-
   return (
     <div className='container about-page'>
       <div className='text-zone'>
         <h1>
           <AnimatedLetters
-            strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
-            idx={15} />
+          strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
+          idx={15}
+          />
         </h1>
-
+        <p> 
+            I'm very ambitious Computer Engineer looking for a role in
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+        </p>
+        <p>
+            I'm quietly confident, naturally curious, and perpetually working on
+            improving my chops one design problem at a time.
+        </p>
+        <p>
+            Result driven System Administrator with 8 Years of experience managing server
+            infrastructures and data-center operations across Windows, Linux, and Mac OS X
+            platforms. Effectively plan, install, configure and optimize IT infrastructures to achieve
+            high availability and performance.
+        </p>
       </div>
     </div>
   )
-
 }
 
 export default About
